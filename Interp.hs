908b3fa5bb6e1bb2ee2c02dca71d9bb5d856dcc9
moonchino <valentin.luna.olariaga@mi.unc.edu.ar>
2025-04-09 00:33:44 +0000

    rot45 encimar

--- Interp.hs
+++ Interp.hs
@@ -34,6 +34,8 @@ interp_espejar f = \v1 v2 v3 -> f (v1 V.+ v2) (V.negate v2) v3
 
 --interpreta el operador de rotacion 45
 interp_rotar45 :: ImagenFlotante -> ImagenFlotante
+interp_rotar45 f = \v1 v2 v3 -> f (v1 V.+ (mitad(v2 V.+ v3))) mitad(v2 V.+ v3) mitad(v3 V.- v2)
+
 
 --interpreta el operador de apilar
 interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
@@ -65,6 +67,8 @@ interp_juntar n m f g = \v1 v2 v3 ->
 
 --interpreta el operador de encimar
 interp_encimar :: ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+inter_encimar f g = \v1 v2 v3 -> f(v1 v2 v3) . g(v1 v2 v3)
+
 
 --interpreta cualquier expresion del tipo Dibujo a
 --utilizar foldDib 
